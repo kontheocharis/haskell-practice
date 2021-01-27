78f203ca21fdf819ba696d117a95c215813999f4
Constantine Theocharis <cthe@mailbox.org>
2021-01-27 14:13:53 +0000

    Add functions and better parsing of neg

--- calculator.hs
+++ calculator.hs
@@ -9,9 +9,6 @@ data BinaryExpr = Add | Subtract | Multiply | Divide | Power
 data BinaryPrec = PowerPrec | MulDivPrec | AddSubPrec
   deriving (Show, Eq)
 
-data Expr = Binary BinaryExpr Expr Expr | Literal Double
-  deriving (Show)
-
 opsForPrec :: BinaryPrec -> [Char]
 opsForPrec PowerPrec = ['^']
 opsForPrec MulDivPrec = ['*', '/']
@@ -24,9 +21,24 @@ exprForOp '*' = Multiply
 exprForOp '/' = Divide
 exprForOp '^' = Power
 
+data FunctionType = Sin | Cos | Tan | Ln | Log | Sqrt
+  deriving (Show, Eq)
+
+funcTypeForLiteral :: [Char] -> Maybe FunctionType
+funcTypeForLiteral "sin" = Just Sin
+funcTypeForLiteral "cos" = Just Cos
+funcTypeForLiteral "tan" = Just Tan
+funcTypeForLiteral "ln" = Just Ln
+funcTypeForLiteral "log" = Just Log
+funcTypeForLiteral "sqrt" = Just Sqrt
+funcTypeForLiteral other = Nothing
+
+data Expr = Binary BinaryExpr Expr Expr | Literal Double | Function FunctionType Expr | Neg Expr
+  deriving (Show)
+
 findFirstOpAcc :: Int -> [Char] -> BinaryPrec -> [Char] -> Maybe (Char, [Char], [Char])
 findFirstOpAcc parensFound acc prec [] = Nothing
-findFirstOpAcc parensFound acc prec input
+findFirstOpAcc parensFound acc prec input@(f:curr)
   | f == '(' = findFirstOpAcc (parensFound + 1) (acc ++ [f]) prec curr
   | f == ')' = findFirstOpAcc (parensFound - 1) (acc ++ [f]) prec curr
   | parensFound == 0
@@ -38,8 +50,6 @@ findFirstOpAcc parensFound acc prec input
       then Nothing
       else Just (f, acc, curr)
   | otherwise = findFirstOpAcc parensFound (acc ++ [f]) prec curr
-  where
-    (f : curr) = input
 
 findFirstOp :: BinaryPrec -> [Char] -> Maybe (Char, [Char], [Char])
 findFirstOp = findFirstOpAcc 0 []
@@ -48,31 +58,50 @@ parseLiteralAcc :: [Char] -> Bool -> [Char] -> Maybe Expr
 parseLiteralAcc acc foundPeriod []
   | last acc /= '.' = Just $ Literal $ read acc
   | otherwise = Nothing
-parseLiteralAcc acc foundPeriod input
-  | f == '-' && not foundPeriod && null acc = parseLiteralAcc (acc ++ [f]) foundPeriod (tail input)
-  | f `elem` ['0' .. '9'] = parseLiteralAcc (acc ++ [f]) foundPeriod (tail input)
-  | f == '.' && acc /= [] && not foundPeriod = parseLiteralAcc (acc ++ [f]) True (tail input)
+parseLiteralAcc acc foundPeriod input@(f:rest)
+  | f `elem` ['0' .. '9'] = parseLiteralAcc (acc ++ [f]) foundPeriod rest
+  | f == '.' && acc /= [] && not foundPeriod = parseLiteralAcc (acc ++ [f]) True rest
   | otherwise = Nothing
-  where
-    f = head input
 
 parseLiteral :: [Char] -> Maybe Expr
 parseLiteral = parseLiteralAcc [] False
 
 parseParens :: [Char] -> Maybe Expr
-parseParens input
-  | f == '(' && t == ')' = (parseExpr . tail . init) input
+parseParens input@(f:rest)
+  | f == '(' && t == ')' = (parseExpr . init) rest
   | otherwise = Nothing
   where
-    f = head input
-    t = last input
+    t = last rest
 
 parseBinary :: BinaryPrec -> [Char] -> Maybe Expr
 parseBinary prec input
-  | Just (op, left, right) <- findFirstOp prec input =
-    case (parseExpr left, parseExpr right) of
-      (Just leftExpr, Just rightExpr) -> Just (Binary (exprForOp op) leftExpr rightExpr)
-      _ -> Nothing
+  | Just (op, left, right) <- findFirstOp prec input = do
+    leftExpr <- parseExpr left
+    rightExpr <- parseExpr right
+    Just $ Binary (exprForOp op) leftExpr rightExpr
+  | otherwise = Nothing
+
+parseFunctionNameAcc :: [Char] -> [Char] -> Maybe ([Char], [Char])
+parseFunctionNameAcc acc [] = Nothing
+parseFunctionNameAcc acc input@(f:rest)
+  | f `elem` ['a' .. 'z'] = parseFunctionNameAcc (acc ++ [f]) rest
+  | null acc = Nothing
+  | otherwise = Just (acc, input)
+
+parseFunctionName :: [Char] -> Maybe ([Char], [Char])
+parseFunctionName = parseFunctionNameAcc []
+
+parseFunction :: [Char] -> Maybe Expr
+parseFunction input
+  | Just (functionName, rest) <- parseFunctionName input = do
+    funcType <- funcTypeForLiteral functionName
+    inner <- parseParens rest
+    Just $ Function funcType inner
+  | otherwise = Nothing
+
+parseNeg :: [Char] -> Maybe Expr
+parseNeg input@(f:rest)
+  | f == '-' = parseExpr rest >>= Just . Neg
   | otherwise = Nothing
 
 parseExpr :: [Char] -> Maybe Expr
@@ -81,6 +110,8 @@ parseExpr input =
     <|> parseBinary MulDivPrec input
     <|> parseBinary PowerPrec input
     <|> parseParens input
+    <|> parseFunction input
+    <|> parseNeg input
     <|> parseLiteral input
 
 parse :: [Char] -> Maybe Expr
@@ -93,6 +124,13 @@ evalExpr (Binary Subtract a b) = evalExpr a - evalExpr b
 evalExpr (Binary Multiply a b) = evalExpr a * evalExpr b
 evalExpr (Binary Divide a b) = evalExpr a / evalExpr b
 evalExpr (Binary Power a b) = evalExpr a ** evalExpr b
+evalExpr (Function Sin x) = sin $ evalExpr x
+evalExpr (Function Cos x) = cos $ evalExpr x
+evalExpr (Function Tan x) = tan $ evalExpr x
+evalExpr (Function Ln x) = logBase (exp 1) $ evalExpr x
+evalExpr (Function Log x) = logBase 10 $ evalExpr x
+evalExpr (Function Sqrt x) = sqrt $ evalExpr x
+evalExpr (Neg x) = - (evalExpr x)
 evalExpr (Literal x) = x
 
 printEval :: [Char] -> Maybe [Char]
